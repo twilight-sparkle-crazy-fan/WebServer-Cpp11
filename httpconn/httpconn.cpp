5b3578daeb5c698a6ffb75c9b2724eddfb2cf820
nailoong <826542246@qq.com>
2025-11-28 16:12:14 +0800

    11/28 update

--- httpconn/httpconn.cpp
+++ httpconn/httpconn.cpp
@@ -1,5 +1,6 @@
 #include "httpconn.h"
 #include <mysql/mysql.h>
+#include <iostream>
 
 //定义http响应的一些状态信息
 constexpr const char *ok_200_title = "OK";
@@ -12,7 +13,7 @@ constexpr const char *error_404_form = "The requested file was not found on this
 constexpr const char *error_500_title = "Internal Error";
 constexpr const char *error_500_form = "There was an unusual problem serving the request file.\n";
 
-std::lock_guard<std::mutex> httpConn::m_lock;
+std::mutex m_lock;
 std::map<std::string,std::string> httpConn::m_user;
 
 void httpConn::initmysql_result(sql_connection_pool *connPool) {
@@ -21,11 +22,13 @@ void httpConn::initmysql_result(sql_connection_pool *connPool) {
 
     if (mysql_query(mysql, "SELECT username,passwd FROM user")) {
         //LOG_ERROR("SELECT error:%s\n", mysql_error(mysql));
+        //TODO
     }
 
     MYSQL_RES *result = mysql_store_result(mysql);
 
-    int num_fields = mysql_num_fields(result);
+    //int num_fields = mysql_num_fields(result);
+    //这行没用，目前
 
     MYSQL_FIELD *fields = mysql_fetch_fields(result);
 
@@ -36,5 +39,106 @@ void httpConn::initmysql_result(sql_connection_pool *connPool) {
     }
 
 }
+
+void addfd(int epollfd, int fd, bool one_shot,int TrigMode){
+    epoll_event event;
+    event.data.fd = fd;
+
+    if (TrigMode == 1)
+        event.events = EPOLLIN | EPOLLET | EPOLLRDHUP;
+    else
+        event.events = EPOLLIN | EPOLLRDHUP;
+
+    if (one_shot)
+        event.events |= EPOLLONESHOT;
+
+    epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &event);
+    setnonblocking(fd);
+}
+
+int setnonblocking(int fd) {
+    int old_option = fcntl(fd, F_GETFL);
+    int new_option = old_option | O_NONBLOCK;
+    fcntl(fd, F_SETFL, new_option);
+    return old_option;
+    // F_GETFL 获取文件描述符的属性
+    // F_SETFL 设置新的状态标志
+}
+
+void removefd(int epollfd, int fd) {
+    epoll_ctl(epollfd, EPOLL_CTL_DEL, fd, 0);
+    close(fd);
+}
+
+void modfd(int epollfd, int fd, int ev,int TrigMode) {
+    epoll_event event;
+    event.data.fd = fd;
+    if (TrigMode == 1)
+        event.events = ev | EPOLLET | EPOLLONESHOT | EPOLLRDHUP;
+    else
+        event.events = ev | EPOLLONESHOT | EPOLLRDHUP;
+
+    epoll_ctl(epollfd, EPOLL_CTL_MOD, fd, &event);
+ }
+
+ std::atomic_int httpConn::m_epollfd {-1};
+ std::atomic_int httpConn::m_userCount {0};
+
+void httpConn::closeConn(bool real_close) {
+    if (real_close && m_sockfd != -1){
+        //std::cout << "closeConn"<< m_sockfd << std::endl;
+        removefd(m_epollfd, m_sockfd);
+        m_sockfd = -1;
+        m_userCount--;
+    }
+}
+
+void httpConn::init(int sockfd, const sockaddr_in& addr,std::string root,int trigMode,int closeLog,std:: string user,std:: string password,std::string sqlname) {
+    m_sockfd = sockfd;
+    m_address = addr;
+    m_userCount ++;
+    m_trigMode = trigMode;
+    m_closeLog = closeLog;
+
+    addfd(m_epollfd, sockfd, true, m_trigMode);
+    
+    docRoot = std::move(root);
+    sqlUser = std::move(user);
+    sqlPassword = std::move(password);
+    sqlName = std::move(sqlname);
+
+    init ();
+ }
+
+
+ void httpConn::init() {
+    mysql = nullptr;
+    bytes_to_send = 0;
+    bytes_have_send = 0;
+    m_checkState = CHECK_STATE_REQUESTLINE;
+    m_linger = false;
+    m_method = GET;
+    m_url = nullptr;
+    m_version = nullptr;
+    m_host = nullptr;
+    m_contentLength = 0;
+
+    m_startLine = 0;
+    m_checkedIdx = 0;
+    m_readIdx = 0;
+
+    cgi = 0;
+    m_state = IO_state::READ;
+    timer_flag = 0;
+    m_processing_finished = false;
+
+    memset(m_readBuf, '\0', READ_BUFFER_SIZE);
+    memset(m_writeBuf, '\0', WRITE_BUFFER_SIZE);
+    memset(m_realFile, '\0', FILENAME_SIZE);
+  }
+
+
+
+
 
 
--- httpconn/httpconn.h
+++ httpconn/httpconn.h
@@ -6,16 +6,23 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <atomic>
+#include <mutex>
+#include <map>
+#include <sys/epoll.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include "../CGImysql/sql_connection_pool.h"
 
-class httpConn {
-    public:
-    static constexpr int FILE_NAME = 200;
-    static constexpr int READ_BUFFER_SIZE  = 2048;
+class httpConn
+{
+public:
+    static constexpr int FILENAME_SIZE = 200;
+    static constexpr int READ_BUFFER_SIZE = 2048;
     static constexpr int WRITE_BUFFER_SIZE = 1024;
-    enum METHOD { 
+    enum METHOD
+    {
         // 请求方法
-        GET, 
+        GET,
         POST,
         // 本项目后面都没用到
         HEAD,
@@ -26,71 +33,104 @@ class httpConn {
         CONNECT,
         PATH
     };
-    enum CHECK_STATE {
-        CHECK_STATE_REQUESTLINE,  //真正分析请求行
-        CHECK_STATE_HEADER,       //分析请求头
-        CHECK_STATE_CONTENT       //分析请求体
+    enum CHECK_STATE
+    {
+        CHECK_STATE_REQUESTLINE, // 真正分析请求行
+        CHECK_STATE_HEADER,      // 分析请求头
+        CHECK_STATE_CONTENT      // 分析请求体
     };
-    enum HTTP_CODE {
-        NO_REQUEST,               //请求不完整，需要继续接收 
-        GET_REQUEST,              //获取完整请求，可调用doRequest()
-        BAD_REQUEST,              //请求语法错误,返回400
-        NO_RESOURCE,              //资源不存在,返回404
-        FORBIDDEN_REQUEST,        //请求的资源被禁止,返回403
-        FILE_REQUEST,             //文件请求,准备返回文件
-        INTERNAL_ERROR,           //内部错误,返回500
-        CLOSED_CONNECTION         //连接被关闭,返回-1
+    enum HTTP_CODE
+    {
+        NO_REQUEST,        // 请求不完整，需要继续接收
+        GET_REQUEST,       // 获取完整请求，可调用doRequest()
+        BAD_REQUEST,       // 请求语法错误,返回400
+        NO_RESOURCE,       // 资源不存在,返回404
+        FORBIDDEN_REQUEST, // 请求的资源被禁止,返回403
+        FILE_REQUEST,      // 文件请求,准备返回文件
+        INTERNAL_ERROR,    // 内部错误,返回500
+        CLOSED_CONNECTION  // 连接被关闭,返回-1
     };
-    enum LINE_STATUS {
-        LINE_OK,                   //行数据完整  因为找到了\r\n
-        LINE_BAD,                  //行数据错误  没到换行符就非法字符，或不符合HTTP协议
-        LINE_OPEN                  //行数据不完整  数据没发完，通过epoll等待下次数据
+    enum LINE_STATUS
+    {
+        LINE_OK,  // 行数据完整  因为找到了\r\n
+        LINE_BAD, // 行数据错误  没到换行符就非法字符，或不符合HTTP协议
+        LINE_OPEN // 行数据不完整  数据没发完，通过epoll等待下次数据
     };
 
-    enum class IO_state {
+    enum class IO_state
+    {
         READ,
         WRITE
     };
 
-    public:
+public:
     httpConn() {};
     ~httpConn() {};
 
-    public:
-    void init(int sockfd, const sockaddr_in& addr,std::string root,int trigMode,int closeLog,std:: string username,std:: string password,std::string sqlname);
-    void closeConn();
+public:
+    void init(int sockfd, const sockaddr_in &addr, std::string root, int trigMode, int closeLog, std::string username, std::string password, std::string sqlname);
+    void closeConn(bool real_close = true);
     void process();
     bool read();
     bool write();
-    sockaddr_in *get_address() {
+    sockaddr_in *get_address()
+    {
         return &m_address;
     }
 
     void initmysql_result(sql_connection_pool *connPool);
     bool timer_flag;
-    std:: atomic_bool m_processing_finished ;
+    std::atomic_bool m_processing_finished;
 
-    private:
+private:
     void init();
     bool process_write(HTTP_CODE ret);
     HTTP_CODE process_read();
     HTTP_CODE do_request();
     HTTP_CODE parse_request_line();
 
-
-
-
-
-    public:
+public:
     static std::atomic_int m_epollfd;
     static std::atomic_int m_userCount;
     MYSQL *mysql;
     IO_state m_state;
 
-    private:
-    int m_sockfd; 
+private:
+    int m_sockfd;
     sockaddr_in m_address;
+
+    static std::mutex m_lock;
+    static std::map<std::string, std::string> m_user;
+
+    std::string docRoot; // 网页根目录
+
+    int m_trigMode;
+    int m_closeLog;
+
+    std::string sqlUser;
+    std::string sqlPassword;
+    std::string sqlName;
+
+    int bytes_to_send;
+    int bytes_have_send;
+
+private:
+    char m_readBuf[READ_BUFFER_SIZE];
+    char m_writeBuf[WRITE_BUFFER_SIZE];
+    char m_realFile[FILE_NAME];
+    CHECK_STATE m_checkState;
+    METHOD m_method;
+    bool m_linger;   // 是否保持连接 默认是关的
+    std::string m_url;
+    std::string m_version;
+    std::string m_host;
+    long m_contentLength;
     
-    
+    int m_startLine;
+    int m_checkedIdx;
+    int m_readIdx;
+    int m_writeIdx;
+    int cgi;  // 是否启用的POST
+
 
 };
